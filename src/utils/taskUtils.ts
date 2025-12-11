f462dbc2c31603673f6ab53f06fb1d966b5597b7
4drea4 <bakerand@outlook.com>
2025-12-11 10:23:28 -0600

    working on troubleshooting the validation

--- src/utils/taskUtils.ts
+++ src/utils/taskUtils.ts
@@ -3,10 +3,10 @@ import type { TaskFormInput } from '../types';
 
 // form validation validate the title,date,description
 export function validationForForm(input:TaskFormInput): string {
-        if (input.title.trim()) {
+        if (!input.title.trim()) {
             return "You have to add a title";
         }
-        if (input.description.trim()){
+        if (!input.description.trim()){
             alert('Let us know the details of your task');
         }
       
