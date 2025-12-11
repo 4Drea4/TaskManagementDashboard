9e7f9af3c880c2ec9894d3249e2b6745d9eb4b42
4drea4 <bakerand@outlook.com>
2025-12-10 21:57:31 -0600

    Screen no longer has error

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -2,26 +2,26 @@ import  React  from 'react';
 // import './App.css';
 import { TaskForm } from './components/TaskForm/TaskForm';
 // import { TaskItem } from './components/TaskList/TaskItem';
-import { TaskFilter } from './components/TaskFilter/TaskFilter';
-import type { TaskFormInput } from '../../types';
+// import { TaskFilter } from './components/TaskFilter/TaskFilter';
+import type { TaskFormInput  } from '../../types';
 
-export const Dashboard: React.FC = () => {
+export const Dashboard = () => {
  const addTask = (formText: TaskFormInput) => {
-    
+    alert("Hey Time to get your life together, lets go!")
  };
 
   return (
     <>
     <h1>Task Dashboard</h1>
 
-    <TaskFilter/>
+    {/* <TaskFilter/> */}
      <TaskForm
      placeholderTitle="Whats your task?"
      placeholderDescription="Tell me what your task is about!"
     onSubmit={addTask}
      />
 
-     <TaskItem/>
+  
      
     </>
   )
