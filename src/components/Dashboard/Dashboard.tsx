9b70e66b6fce453af4f04ff88099fbf6962d9311
4drea4 <bakerand@outlook.com>
2025-12-10 18:54:52 -0600

    coming back to the dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,9 +1,32 @@
 // form and task item and item list/filter
-
 import { useState } from "react";
 import type { TaskFormProps , TaskFormInput } from "../../types";
 
-export const TaskForm: React.FC = () => {
-    const [form, setForm] = useState<TaskFormProps>('')
-    
+export const TaskForm: React.FC<TaskFormProps> = ({
+    placeholderTitle,
+    placeholderDescription,
+    onSubmit,
+})=> {
+
 }
+
+       
+
+
+return (
+    <form 
+    <form onSubmit={handleSubmit}>
+        <div>
+        <label htmlFor="name"
+        <input 
+        type='text'
+        placeholder=
+        />
+ </form>
+     }
+
+
+
+        </div>
+   
+)
