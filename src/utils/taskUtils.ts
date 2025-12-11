d3a87d693c6a69046d90648886b0ad4024bc2cda
4drea4 <bakerand@outlook.com>
2025-12-11 10:12:52 -0600

    adding form validation

--- src/utils/taskUtils.ts
+++ src/utils/taskUtils.ts
@@ -1,7 +1,15 @@
 import React, {useState} from 'react';
+import type { TaskFormInput } from '../types';
 
 // form validation validate the title,date,description
-
+export function validationForForm(input:TaskFormInput): string {
+        if (input.title.trim()) {
+            return "You have to add a title";
+        }
+       else if (input.description.trim()){
+            alert('Let us know the details of your task');
+        }
+}
 
 
 //function for having task input item key value pairs to be local storage
