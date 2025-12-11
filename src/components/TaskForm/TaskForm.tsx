53f8947cc4459f20ae5b621ac79a4d090c179b58
4drea4 <bakerand@outlook.com>
2025-12-10 19:45:16 -0600

    I realized i was defining the entire taskform in the dashboard comp so i just moved it over

---
+++ src/components/TaskForm/TaskForm.tsx
@@ -0,0 +1,55 @@
+// form and task item and item list/filter
+import { useState } from "react";
+import type { TaskFormProps , TaskFormInput } from "../../types";
+
+//taskform component with type of TaskFormProps
+export const TaskForm: React.FC<TaskFormProps> = (props)=> { 
+
+//setting state on form 
+const [form, setForm] = useState<TaskFormInput>({
+
+    title:"",
+    description:"",
+    dueDate: "",
+});
+//handle change on task field
+const
+
+
+//handlesubmit
+const handleSubmit = (event: React.FormEvent<HTMLFormElement> ) =>{
+    event.preventDefault();
+    props.onSubmit(form);
+    
+}
+
+)
+//handlesubmit
+
+//
+       
+return (
+   
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="Title">Title</label>
+         <input 
+             type='text'
+             placeholder=""
+             onChange={handleChange}
+        />
+         </div>
+
+         <div>
+            <label htmlFor="Description">Description</label>
+                <input
+
+
+                />
+
+         </div>
+     </form>
+
+)  
+    
+}
